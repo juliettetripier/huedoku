cb80708328126538745bd4cb71fb8b11ab7a55d9
Juliette Tripier <juli.tripier@gmail.com>
2024-08-15 13:58:19 -0400

    Added popovers to each tile

--- app/components/tile.tsx
+++ app/components/tile.tsx
@@ -1,5 +1,27 @@
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor,
+  } from '@chakra-ui/react'
+
 export function Tile() {
-    return <div className="tile">
-        <h1>Test</h1>
-    </div>;
+    return <Popover>
+            <PopoverTrigger>
+                <div className="tile">
+                    <h1>Test</h1>
+                </div>
+            </PopoverTrigger>
+            <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Header</PopoverHeader>
+                <PopoverBody>Body</PopoverBody>
+            </PopoverContent>
+        </Popover>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
-import Board from "./components/grid";
+import { ChakraProvider } from '@chakra-ui/react'
+import Board from "./components/board";
 import { Tile } from "./components/tile";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Board>
-          <Tile />
-        </Board>
-      </div>
-    </main>
+    <ChakraProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
+          <Board>
+            <Tile />
+          </Board>
+        </div>
+      </main>
+    </ChakraProvider>
   );
 }
