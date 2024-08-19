44dfcb29f3773c1359f32da104f624c776f3c54e
Juliette Tripier <juli.tripier@gmail.com>
2024-08-19 13:45:30 -0400

    Popovers now close automatically when a color option is clicked

--- app/components/tile.tsx
+++ app/components/tile.tsx
@@ -8,6 +8,7 @@ import {
     PopoverArrow,
     PopoverCloseButton,
     PopoverAnchor,
+    useDisclosure,
   } from '@chakra-ui/react'
 import { Dispatch, SetStateAction, useState } from 'react';
 import { ColorOption } from './coloroption';
@@ -16,11 +17,12 @@ type TileProps = {
     palette: Array<string>;
 }
 
-function changeTileColor(setColor: Dispatch<SetStateAction<string>>, color: string) {
+function changeTileColor(setColor: Dispatch<SetStateAction<string>>, color: string, onClose: () => void) {
     setColor(color);
+    onClose();
 }
 
-function generateColorOptions(props: TileProps, setColor: Dispatch<SetStateAction<string>>) {
+function generateColorOptions(props: TileProps, setColor: Dispatch<SetStateAction<string>>, onClose: () => void) {
     const colorOptions = [];
     const colorPalette = props.palette;
     const numOptions = 9;
@@ -30,7 +32,7 @@ function generateColorOptions(props: TileProps, setColor: Dispatch<SetStateActio
             <ColorOption 
                 key={i} 
                 color={colorPalette[i]} 
-                onColorSelection={() => changeTileColor(setColor, colorPalette[i])}/>);
+                onColorSelection={() => changeTileColor(setColor, colorPalette[i], onClose)}/>);
         }
 
     return colorOptions;
@@ -38,10 +40,11 @@ function generateColorOptions(props: TileProps, setColor: Dispatch<SetStateActio
 
 export function Tile(props: TileProps) {
     const [color, setColor] = useState<string>('transparent');
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return <Popover>
+    return <Popover isOpen={isOpen} onClose={onClose}>
             <PopoverTrigger>
-                <div className="tile" style={{ backgroundColor: color }}>
+                <div className="tile" style={{ backgroundColor: color }} onClick={onOpen}>
                     <h1>Test</h1>
                 </div>
             </PopoverTrigger>
@@ -51,7 +54,7 @@ export function Tile(props: TileProps) {
                 <PopoverHeader>Color Picker</PopoverHeader>
                 <PopoverBody style={{display: 'flex'}}>
                     <div className="color-option-grid">
-                        {generateColorOptions(props, setColor)}
+                        {generateColorOptions(props, setColor, onClose)}
                     </div>
                 </PopoverBody>
             </PopoverContent>
